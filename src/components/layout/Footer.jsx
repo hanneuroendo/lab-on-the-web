9c13f306ff9f7af12588c07720090de63c9aada7
holyseo <128103221+holyseo@users.noreply.github.com>
2026-05-25 17:51:56 +0100

    Update X/Twitter profile URL in footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ import logo from "../../assets/logo-dark.svg";
 const SOCIALS = [
   {
     label: "X / Twitter",
-    href: "https://x.com/hanlabneuroendo",
+    href: "https://x.com/hanneuroendo",
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.746l7.73-8.835L1.254 2.25H8.08l4.253 5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
